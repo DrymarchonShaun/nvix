e968c72a70e932fcdaaa24fa986af57792c0a643
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-08-07 16:32:40 -0700

    feat: try nixd

--- config/lang/nix.nix
+++ config/lang/nix.nix
@@ -2,20 +2,22 @@
   plugins = {
     nix.enable = true;
     nix-develop.enable = true;
-    lsp.servers.nil-ls = {
-      enable = true;
-      package = inputs.nil.packages.${pkgs.system}.default;
-      extraOptions = {
-        nix = {
-          maxMemoryMB = 0;
-          flake = {
-            autoArchive = lib.mkDefault true;
-            autoEvalInputs = lib.mkDefault true;
-            nixpkgsInputName = "nixpkgs";
-          };
-        };
-      };
-    };
+    /* lsp.servers.nil-ls = {
+             enable = true;
+             package = inputs.nil.packages.${pkgs.system}.default;
+             extraOptions = {
+               nix = {
+                 maxMemoryMB = 0;
+                 flake = {
+                   autoArchive = lib.mkDefault true;
+                   autoEvalInputs = lib.mkDefault true;
+                   nixpkgsInputName = "nixpkgs";
+                 };
+               };
+             };
+       };
+    */
+    lsp.servers.nixd.enable = true;
     none-ls = {
       sources = {
         formatting.nixfmt.enable = true;
