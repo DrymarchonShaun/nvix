61d1bf1a1976474b94c0a7c9e4dc8143ab590eb8
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-02-02 12:55:26 -0800

    feat: add openscad lang

---
+++ modules/lang/openscad.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  plugins = {
+    openscad = {
+      enable = true;
+    };
+    lsp.servers.openscad_lsp = {
+      enable = true;
+      settings = {
+        fmt_exe = "${pkgs.clang-tools}/bin/clang-format";
+        fmt-style = "file";
+      };
+    };
+  };
+}
