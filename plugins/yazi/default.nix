fbf3239c95b38743fa57edd9bba265d83b4ae52c
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-10-24 11:14:18 -0700

    feat: add yazi intergration

---
+++ plugins/yazi/default.nix
@@ -0,0 +1,12 @@
+{ lib, ... }:
+{
+  plugins.yazi = {
+    enable = true;
+  };
+  imports =
+    with builtins;
+    with lib;
+    map (fn: ./${fn}) (
+      filter (fn: (fn != "default.nix" && !hasSuffix ".md" "${fn}")) (attrNames (readDir ./.))
+    );
+}
